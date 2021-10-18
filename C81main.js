5dd9963ccf26fb40a685d62a03d3a002f03f97dc
Aarushfr <92362446+Aarushfr@users.noreply.github.com>
2021-10-18 19:49:36 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,25 @@
+canvas= document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="red";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke0();
+
+function circle(mouse_x,mouse_y);
+{
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+ctx.stroke0();
+}
+
+
+function my_mouseDown(e)
+{
+
+    
+}
